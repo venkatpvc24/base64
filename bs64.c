a2e96779ec04d40cd4995e68fd4a09505f8bf7f8
venkatpvc24 <79187594+venkatpvc24@users.noreply.github.com>
2021-09-27 18:56:22 +0530

    Update bs64.c
    added line length and corrected errors

--- bs64.c
+++ bs64.c
@@ -7,7 +7,8 @@
 
 #define MAX_LINE 10
 
-static void b64_encode(unsigned char* in, unsigned char* out)
+
+static void b64_encode(const char in[3], unsigned char* out, int len)
 {
     char* base64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
@@ -19,8 +20,8 @@ static void b64_encode(unsigned char* in, unsigned char* out)
 
     out[0] = base64[(unsigned char)((data_3b >>  3 * 6) & 0x3f)];
     out[1] = base64[(unsigned char)((data_3b >>  2 * 6) & 0x3f)];
-    out[2] = base64[(unsigned char)((data_3b >>  1 * 6) & 0x3f)];
-    out[3] = base64[(unsigned char)((data_3b >>  0 * 6) & 0x3f)];
+    out[2] = len == 1 ? '=' : base64[(unsigned char)((data_3b >>  1 * 6) & 0x3f)];
+    out[3] = len == 1 || len == 2 ? '=' : base64[(unsigned char)((data_3b >>  0 * 6) & 0x3f)];
 
 }
 
@@ -29,6 +30,7 @@ int base64(int argc, char** argv)
     bool encode = false, decode = false;
     char *in_file = NULL, *out_file = NULL;
     FILE *in, *out;
+    size_t line_length = 0;
     for ( int i = 0; i < argc; i++)
     {
         if (argv[i][0] == '-')
@@ -51,6 +53,20 @@ int base64(int argc, char** argv)
                     exit(0);
                 }
                 out_file = argv[i+1];
+            }
+            if (strcmp(argv[i], "-max-len"))
+            {
+                int len;
+                sscanf(argv[i], "%d", &len);
+
+                if (len > MAX_LINE)
+                {
+                    line_length = len;
+                }
+                else
+                {
+                    line_length = MAX_LINE;
+                }
             }
         }
     }
@@ -79,29 +95,26 @@ int base64(int argc, char** argv)
             out = stdout;
         }
 
-        size_t line = 0;
-        int c, n = 0, count = 0;
-        while((c = fgetc(in)) != EOF) {
-            if (count < 3)
+        int c, count = 0;
+        while(!feof(in)) {
+            while( count < 3)
             {
-                data_in[n++] = (char)c;
+                if ((c = fgetc(in)) == EOF) break;
+                data_in[count] = (char)c;
                 count++;
             }
-            if (count > 2)
-            {
-                b64_encode(data_in, data_out);
-                // printf("%s\n", data_out);
-                if ( line < 76 ) {
-                    fwrite(data_out, 1, 4, out);
-                }
-                if (line == 72 )
-                {
-                    fputs("\r\n", out); line = 0;
-                }
-                line += 4;
-                count = 0;
-                n = 0;
+            // int len = count_filled_bytes(data_in);
+            b64_encode(data_in, data_out, count);
+            memset(data_in, 0, sizeof data_in);
+            if ( line_length < 72 ) {
+                fwrite(data_out, 1, 4, out);
             }
+            if (line_length >= 72 )
+            {
+                fputs("\r\n", out); line_length = 0;
+            }
+            line_length += 4;
+            count = 0;
         }
     }
 
